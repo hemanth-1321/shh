3aced582b85d61f64d1600c0b2281ea023e87323
hemanth <hemanth02135@gmail.com>
2025-06-29 22:43:36 +0530

    config

--- client/src/config/Api.ts
+++ client/src/config/Api.ts
@@ -1,3 +1,5 @@
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8080";
-export const NEXT_URL = process.env.NEXT_URl || "http://localhost:3000";
-export const WS_URL = process.env.BACKEND_URL || "ws://localhost:8080";
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
+export const NEXT_URL =
+  process.env.NEXT_PUBLIC_NEXT_URL || "http://localhost:3000";
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
