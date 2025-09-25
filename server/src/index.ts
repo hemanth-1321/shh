5189bd81c7acbef43a7439f985799bec30d76922
hemanth-2113 <hemanth02135@gmail.com>
2025-09-25 11:11:51 +0530

    adde

--- server/src/index.ts
+++ server/src/index.ts
@@ -56,5 +56,5 @@ app.use("/message", MessageRoute);
 
 // Start server
 server.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on  ${PORT}`);
 });
